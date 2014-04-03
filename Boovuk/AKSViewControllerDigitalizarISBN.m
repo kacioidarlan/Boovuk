f3ab05c90cbddf0330a8ba258451bf2575c94dc6
Kácio Idarlan Oliveira de Souza <kio_idarlan@hotmail.com>
2014-04-03 13:09:26 -0300

    Adicionado segue da digitalização para formulário

--- Boovuk/AKSViewControllerDigitalizarISBN.m
+++ Boovuk/AKSViewControllerDigitalizarISBN.m
@@ -59,16 +59,15 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - Navigation
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    if ([[segue identifier] isEqualToString:@"segueFormulario"]) {
+        AKSViewControllerFormulario *formularioViewController = (AKSViewControllerFormulario *)[segue destinationViewController];
+        formularioViewController.managedObjectContext = self.managedObjectContext;
+        formularioViewController.livroIncluir = sender;
+    }
 }
-*/
 
 #pragma mark - Captura de Video
 
